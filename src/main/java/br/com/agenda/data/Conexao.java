28d15f9a3512f81a74461e44490cd24506cad111
Adriano Drozdek <61703@PROWAY.TREINA>
2020-11-01 17:05:45 -0300

    Implementação dos metodos de conexão com o MySql

--- src/main/java/br/com/agenda/data/Conexao.java
+++ src/main/java/br/com/agenda/data/Conexao.java
@@ -1,5 +1,19 @@
 package br.com.agenda.data;
 
-public class Conexao {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface Conexao {
+	
+	Connection getConnection();
+		
+	void close();
+	
+	void commit() throws SQLException;
+	
+	void rollback();
+	
+		
+	
 
 }
