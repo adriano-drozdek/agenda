ec5e4da28d05303f6afec31a2b2f1e375121e5ee
Adriano Drozdek <61703@PROWAY.TREINA>
2020-11-01 18:57:16 -0300

    Metodo de inserção tabela local

---
+++ src/main/java/br/com/agenda/teste/LocalTeste.java
@@ -0,0 +1,13 @@
+package br.com.agenda.teste;
+
+import java.sql.SQLException;
+
+import br.com.agenda.dao.LocalDAO;
+
+public class LocalTeste {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		LocalDAO dao = new LocalDAO();
+		dao.inserir("Casa");
+	}
+}
